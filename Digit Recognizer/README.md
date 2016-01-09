a33a5c3578d741f873be6f7f9a99197b95ed8281
Your Name <you@example.com>
2016-01-09 14:09:37 +0800

    .

--- Digit Recognizer/README.md
+++ Digit Recognizer/README.md
@@ -7,7 +7,7 @@ An image recognition contest whose challenge was to classify handwritten single 
 *  [Data](https://www.kaggle.com/c/digit-recognizer/data)
 	
 #Notebook :
-Please see Notebook (in progress) for step-by-step tackling the contest
+Please see [Notebook](http://nbviewer.ipython.org/gist/tranlyvu/da54146d97147cb401c4) for step-by-step tackling the contest
 	
 #Source code:
 Source code can be found [here](https://github.com/tranlyvu/kaggle/tree/master/Digit%20Recognizer/main)
--- Digit Recognizer/main/first_attempt.py
+++ Digit Recognizer/main/first_attempt.py
@@ -1,3 +1,12 @@
+"""
+	The script generate submission file for the kaggle contest "Digit recognizer",An
+	image recognition contest whose challenge was to classify handwritten single digits
+
+	dimensionality reduction with PCA
+	Predictive model: Kmeans
+"""
+
+
 import pandas as pd
 import numpy as np
 from scipy import cluster
@@ -5,17 +14,19 @@ from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
-if __name__ == '__main__':
-	print "load training dataset..."
-	train=pd.read_csv('...Digit_Recognizer/train.csv')
+def main():
+	#loading original train dataset
+	train=pd.read_csv('/train.csv')
 
-	print "extracting features from original training set..."
+	#features from training set
 	train_data=train.values[:,1:]
+	
+	#targetf rom training set
 	train_target=train.ix[:,0]
 
-	print "initiating PCA..."
-	pca=PCA(n_components=0.90,whiten=True)
-	train_data_pca=pca.fit_transform(train_data)
+	#pro-processing train features
+	my_pca=PCA(n_components=0.90,whiten=True)
+	train_data_pca=my_pca.fit_transform(train_data)
 
 	print "using elbow method to select number of clusters in KMeans model...."
 	initial = [cluster.vq.kmeans(train_data_pca,i) for i in range(1,10)]
@@ -28,21 +39,26 @@ if __name__ == '__main__':
 	plt.title('Elbow for K-means clustering')
 	plt.show()
 
-	print "initiating KMean model using number of cluster from Elbow plot..."
-	Kmean=KMeans(init='k-means++',n_clusters=6)
+	# training Kmean
+	model=KMeans(init='k-means++',n_clusters=6)
 
-	print "training data from original training set..."
+	#fitting the model
 	Kmean.fit(train_data_pca,train_target)
 
-	print "loading original test set..."
-	test=pd.read_csv('...Digit_Recognizer/test.csv')
+	#loading original test dataset
+	test_data=pd.read_csv('/test.csv')
 		
-	print "extract and transform test set data..."
-	submit_data=test.values
-	submit_data_pca=pca.transform(submit_data)
+	#features from test data
+	test_features=test_data.values
+	
+	#pro-processing test features
+	pca_test_features=my_pca.transform(test_features)
 
-	print "predicting original test set..."	
-	prediction=Kmean.predict(submit_data_pca)
+	#predicting from test set
+	prediction=model.predict(pca_test_features)
 
-	print "writing out submission file..."
+	#preparing submission file
 	pd.DataFrame({"ImageId": range(1,len(prediction)+1), "Label": prediction}).to_csv('K-means.csv', index=False, header=True)
+
+if __name__=="__main__":
+	main()
